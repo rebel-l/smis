d4a15e4e3bc7ed5557e9c371b665ab9ebe82ddb7
Lars Gaubisch <l.gaubisch@googlemail.com>
2019-08-05 13:58:47 +0200

    added test for registring endpoint

--- smis_test.go
+++ smis_test.go
@@ -1,6 +1,110 @@
 package smis
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/rebel-l/go-utils/slice"
+
+	"github.com/gorilla/mux"
+)
+
+func TestService_RegisterEndpoint(t *testing.T) {
+	testcases := []struct {
+		name   string
+		path   string
+		method string
+		err    error
+	}{
+		{
+			name: "not allowed method",
+			err:  fmt.Errorf("method  is not allowed"),
+		},
+		{
+			name:   "method connect",
+			path:   "/health",
+			method: http.MethodConnect,
+		},
+		{
+			name:   "method delete",
+			path:   "/health",
+			method: http.MethodDelete,
+		},
+		{
+			name:   "method get",
+			path:   "/health",
+			method: http.MethodGet,
+		},
+		{
+			name:   "method head",
+			path:   "/health",
+			method: http.MethodHead,
+		},
+		{
+			name:   "method options",
+			path:   "/health",
+			method: http.MethodOptions,
+		},
+		{
+			name:   "method patch",
+			path:   "/health",
+			method: http.MethodPatch,
+		},
+		{
+			name:   "method post",
+			path:   "/health",
+			method: http.MethodPost,
+		},
+		{
+			name:   "method put",
+			path:   "/health",
+			method: http.MethodPut,
+		},
+		{
+			name:   "method trace",
+			path:   "/health",
+			method: http.MethodTrace,
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			service := Service{Router: mux.NewRouter()}
+			route, err := service.RegisterEndpoint(
+				testcase.path,
+				testcase.method,
+				func(_ http.ResponseWriter, _ *http.Request) {},
+			)
+
+			if err != nil && testcase.err != nil {
+				if err.Error() != testcase.err.Error() {
+					t.Errorf("Expected the error '%s' but got '%s'", testcase.err, err)
+				}
+				return
+			}
+
+			// check methods
+			var methods slice.StringSlice
+			methods, err = route.GetMethods()
+			if err != nil {
+				t.Fatal("cannot retrieve from created route")
+			}
+			if methods.IsNotIn(testcase.method) {
+				t.Errorf("method '%s' is not set on route", testcase.method)
+			}
+
+			// check registered endpoints
+			if registeredEndpoints.KeyNotExists(testcase.path) {
+				t.Errorf("path '%s' is not added to registred endpoints", testcase.path)
+			}
+
+			if registeredEndpoints.GetValuesForKey(testcase.path).IsNotIn(testcase.method) {
+				t.Errorf("method '%s' is not set added to registered endpoints", testcase.method)
+			}
+		})
+	}
+}
 
 func TestExtractPath(t *testing.T) {
 	tests := []struct {
