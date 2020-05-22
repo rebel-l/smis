427e7af41d32c7ca58933c1cd03156365c2b89d7
rebel-l <dj@rebel-l.net>
2020-05-22 23:59:50 +0200

    charset to content-type JSON

--- body.go
+++ body.go
@@ -15,10 +15,21 @@ var (
 	ErrParseBody = errors.New("failed to parse body")
 )
 
+func isValidJSONHeader(header http.Header) bool {
+	// RFC 4627 defines HeaderContentTypeJSON to be the right header but some APIs send HeaderContentTypeJavascript
+	if header == nil ||
+		(!strings.Contains(header.Get(HeaderKeyContentType), HeaderContentTypeJSON) &&
+			!strings.Contains(header.Get(HeaderKeyContentType), HeaderContentTypeJavascript)) {
+		return false
+	}
+
+	return true
+}
+
 // ParseJSONRequestBody takes a request and parses the JSON body to the destination value.
 // If it is not parsable it returns ErrParseBody.
 func ParseJSONRequestBody(request *http.Request, destination interface{}) error {
-	if request.Header == nil || !strings.Contains(request.Header.Get(HeaderKeyContentType), HeaderContentTypeJSON) {
+	if !isValidJSONHeader(request.Header) {
 		return fmt.Errorf("%w: body is not a JSON", ErrParseBody)
 	}
 
@@ -28,7 +39,7 @@ func ParseJSONRequestBody(request *http.Request, destination interface{}) error 
 // ParseJSONResponseBody takes a response and parses the JSON body to the destination value.
 // If it is not parsable it returns ErrParseBody.
 func ParseJSONResponseBody(response *http.Response, destination interface{}) error {
-	if response.Header == nil || !strings.Contains(response.Header.Get(HeaderKeyContentType), HeaderContentTypeJSON) {
+	if !isValidJSONHeader(response.Header) {
 		return fmt.Errorf("%w: body is not a JSON", ErrParseBody)
 	}
 
